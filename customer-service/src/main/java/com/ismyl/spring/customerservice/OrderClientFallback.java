4f0e27e8406f2d105f03b261309b2212ec041299
ismyl <ismyl.awn@gmail.com>
2020-02-02 01:37:46 +0900

    - OrderClient fallback 처리
    return emptyList

---
+++ customer-service/src/main/java/com/ismyl/spring/customerservice/OrderClientFallback.java
@@ -0,0 +1,14 @@
+package com.ismyl.spring.customerservice;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class OrderClientFallback implements OrderClient {
+
+    @Override
+    public Object getOrdersForCustomer(int customerId) {
+        return Collections.emptyList();
+    }
+}
